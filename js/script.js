ab4539af894b19418bf1241b5c25a6079d3c6795
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-17 15:50:37 +0100

    bonus 1

--- js/script.js
+++ js/script.js
@@ -3,16 +3,33 @@ let app = new Vue (
         el: "#root",
         data: {
             albums: [],
+            genreSelected: "All",
         },
         methods: {
-
+            getGenreFromArray: function () {
+                let newArray = [];
+                this.albums.forEach( (element) => {
+                    if (!newArray.includes(element.genre)) {
+                        newArray.push(element.genre);
+                    }
+                } );
+                return newArray
+            },
+            filterAlbumsForGenre: function () {
+                let newArray = [];
+                this.albums.forEach( (element) => {
+                    if (element.genre == this.genreSelected) {
+                        newArray.push(element);
+                    }
+                } );
+                return newArray;
+            }
         },
         mounted: function () {
             axios
                 .get("https://flynn.boolean.careers/exercises/api/array/music")
                 .then( (result) => {
                     this.albums = result.data.response;
-                    console.log(this.albums);
                 } );
         }
     }
